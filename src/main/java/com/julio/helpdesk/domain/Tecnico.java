2fab4741e59299ea2ef0290765a67466cc2ba6bb
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-14 19:59:13 -0300

    Feat: anotações jpa para classes de dominio

--- src/main/java/com/julio/helpdesk/domain/Tecnico.java
+++ src/main/java/com/julio/helpdesk/domain/Tecnico.java
@@ -3,12 +3,23 @@ package com.julio.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.julio.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa{
 	
+
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.TECNICO);
 	
 	}
 
