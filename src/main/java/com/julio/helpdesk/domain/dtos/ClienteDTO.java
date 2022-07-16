51e34480d96e7eb78aa3aaae3ba7229690c60476
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-16 15:52:08 -0300

    Feat: CRUD para cliente implementado

--- src/main/java/com/julio/helpdesk/domain/dtos/ClienteDTO.java
+++ src/main/java/com/julio/helpdesk/domain/dtos/ClienteDTO.java
@@ -9,10 +9,10 @@ import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.julio.helpdesk.domain.Tecnico;
+import com.julio.helpdesk.domain.Cliente;
 import com.julio.helpdesk.domain.enums.Perfil;
 
-public class TecnicoDTO implements Serializable{
+public class ClienteDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -31,12 +31,12 @@ public class TecnicoDTO implements Serializable{
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 
-	public TecnicoDTO() {
+	public ClienteDTO() {
 		super();
 		addPerfis(Perfil.CLIENTE);
 	}
 
-	public TecnicoDTO(Tecnico obj) {
+	public ClienteDTO(Cliente obj) {
 		super();
 		this.id = obj.getId();
 		this.nome = obj.getNome();
