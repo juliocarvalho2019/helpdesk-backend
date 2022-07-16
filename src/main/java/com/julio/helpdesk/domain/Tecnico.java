265f1d0dd9f0fe317240db1129ea65a717d4fe22
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-07-16 09:54:14 -0300

    Feat: feito implementação create tecnico

--- src/main/java/com/julio/helpdesk/domain/Tecnico.java
+++ src/main/java/com/julio/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.julio.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.julio.helpdesk.domain.dtos.TecnicoDTO;
 import com.julio.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -21,13 +23,25 @@ public class Tecnico extends Pessoa{
 
 	public Tecnico() {
 		super();
-		addPerfis(Perfil.TECNICO);
+		addPerfis(Perfil.CLIENTE);
 	
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
 		
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/julio/helpdesk/resources/TecnicoResouce.java
+++ src/main/java/com/julio/helpdesk/resources/TecnicoResouce.java
@@ -1,14 +1,19 @@
 package com.julio.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.julio.helpdesk.domain.Tecnico;
 import com.julio.helpdesk.domain.dtos.TecnicoDTO;
@@ -33,6 +38,14 @@ public class TecnicoResouce {
 	public ResponseEntity<List<TecnicoDTO>> findAll() {
 		List<Tecnico> list = service.findAll();
 	    List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
-	    return ResponseEntity.ok().body(listDTO);	}
+	    return ResponseEntity.ok().body(listDTO);	
+   }
+	@PostMapping
+   public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO){
+	Tecnico newObj = service.create(objDTO);
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+	return ResponseEntity.created(uri).build();		
+	
+   }
 
 }
